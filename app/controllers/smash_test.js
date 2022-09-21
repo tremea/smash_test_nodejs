7255eabcfdcdbc824701a02158e6c2b04579aa52
Rafael Tremea <rafael.tremea@gmail.com>
2022-09-21 10:33:54 -0300

    estrutura firebase

--- app/controllers/smash_test.js
+++ app/controllers/smash_test.js
@@ -1,35 +1,107 @@
-const db = require('/Desenvolvimento/smash/node-js-smash-test/config')
 
 
-  exports.smash_test = async (req, res, next) => {
+const db = require('/Desenvolvimento/smash/smash-test/config')
+const express = require('express')
 
-    const csvFilePath='world-cities.csv'
-    const csv=require('csvtojson')
-    csv()
+
+exports.smash_test = async (req, res, next) => {
+
+  const csvFilePath = 'world-cities.csv'
+  const csv = require('csvtojson')
+  csv()
     .fromFile(csvFilePath)
-    .then(async (jsonObj)=>{
-        console.log(jsonObj);
-        
-    
-    const Teste = db.collection("smash-teste");
-    await Teste.add(jsonObj[0]);
-        /**
-         * [
-         * 	{a:"1", b:"2", c:"3"},
-         * 	{a:"4", b:"5". c:"6"}
-         * ]
-         */ 
+    .then(async (jsonObj) => {
+
+
+      var listAdd = [];
+      listAdd.push(jsonObj[0]);
+      var auxCountry = jsonObj[0].country;
+
+
+      for (var i = 1; i < jsonObj.length; i++) {
+        if (auxCountry == jsonObj[i].country) {
+          listAdd.push(jsonObj[i]);
+        } else {
+
+          db.collection("world-cities").doc(auxCountry).set({"countries" : listAdd}).then(() => {
+            console.log("Elementos inseridos");
+          });
+          listAdd = [];
+          listAdd.push(jsonObj[i]);
+          auxCountry = jsonObj[i].country;
+
+
+        }
+        console.log(jsonObj[i].country);
+      }
+
+
+      /*db.collection("world-cities").doc("world-cities").set({ "world-cities": jsonObj }).then(() => {
+        console.log("Elementos inseridos");
+      });*/
+
+
     })
-     
-    // Async / await usage
-    const jsonArray=await csv().fromFile(csvFilePath);
 
 
-    
+  res.status(200).send({
+    success: true
+  });
+};
 
-    res.status(200).send({
-      success: true
+
+exports.smash_test_get = async (req, res, next) => {
+
+
+  const world_cities = db.collection('world-cities');//.doc('world-cities');
+
+  const doc = await world_cities.get();
+  if (!doc.exists) {
+    return res.status(200).send({
+      success: false,
+      data: doc.data()
     });
-  };
+  } else {
+    console.log('Document data:', doc.data());
+    return res.status(200).send({
+      success: true,
+      data: doc.data()
+    });
+  }
 
- 
+
+
+
+};
+
+exports.smash_test_get_country = async (req, res, next) => {
+
+
+
+  const world_cities = db.collection('world-cities');//.doc('world-cities');
+  const doc = await world_cities.get();
+
+
+
+
+  if (!doc.exists) {
+    return res.status(200).send({
+      success: false,
+      data: doc.id
+    });
+  } else {
+
+    doc.forEach(element => {
+      console.log('Element ' + element)
+    });
+
+    return res.status(200).send({
+      success: true,
+      data: doc.id
+    });
+  }
+
+
+
+
+};
--- app/routes/smash_test.js
+++ app/routes/smash_test.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/smash_test')
 
-router.post('/', controller.smash_test);
+router.post('/inserirDoc', controller.smash_test);
+router.get('/getDoc', controller.smash_test_get);
+router.get('/getDocCountry', controller.smash_test_get_country);
 
 module.exports = router;
 
